bed5a32d172177c15b5a00c0514f37a280164d5f
Ahad J <ahadjahangir@live.com>
2021-07-06 16:42:58 -0700

    Updating documentation 2

--- flaskblog/errors/handlers.py
+++ flaskblog/errors/handlers.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 errors = Blueprint('errors', __name__)
 
+#Error handling for 404(Not Found), 403(Forbidden), 500 (Internal Server Error)
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html'), 404
